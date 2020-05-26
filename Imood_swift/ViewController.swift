08f49cb69bb241a367fe7e4cd491599e2f8f949d
mal666666 <mal666666@163.com>
2020-05-26 18:15:26 +0800

    界面搭建

--- Imood_swift/ViewController.swift
+++ Imood_swift/ViewController.swift
@@ -14,23 +14,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.white
-        //
+        self.edgesForExtendedLayout = UIRectEdge.bottom
+        //背景
         let backgroundIV :UIImageView = UIImageView.init(image: UIImage.init(named: "background"))
         self.view.addSubview(backgroundIV)
         //backgroundIV.frame = CGRect(x: 0,y: 0,width: 330,height: 330)
         backgroundIV.mas_makeConstraints { (make) in
             make?.height.equalTo()(self.view.mas_width)
-            make?.topMargin.offset()(10)
+            make?.topMargin.equalTo()(self.view)
             make?.left.right()?.offset()(0)
         }
-        //
+        //选照片滑条
         let scroll = UIScrollView.init()
         self.view.addSubview(scroll)
-//        scroll.mas_makeConstraints { (make) in
-//            make?.height.equalTo()(self.view.mas_width)
-//            //make?.top.equalTo(backgroundIV)
-//            make?.left.right()?.offset()(0)
-//        }
+        scroll.backgroundColor = UIColor.init(red: 0.2, green: 0.42, blue: 0.54, alpha: 1.0)
+        scroll.mas_makeConstraints { (make) in
+            make?.height.offset()(80)
+            make?.top.equalTo()(backgroundIV.mas_bottom)
+            make?.left.right()?.offset()(0)
+        }
         
     }
 
